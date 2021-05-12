b943921bf799dbdf77d4f863c3786519280e8fc3
tobi00604 <>
2021-05-12 18:22:17 +0900

    A - CODE FESTIVAL 2014

---
+++ src/code_festival_2014_quala/A/Main.java
@@ -0,0 +1,13 @@
+package code_festival_2014_quala.A;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next() + "2014");
+
+	}
+}
