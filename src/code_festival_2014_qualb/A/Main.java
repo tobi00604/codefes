069cb3e72c73829eeae4332a1ed564de31c5a99b
tobi00604 <>
2021-05-13 17:31:23 +0900

    A - あるピアニスト

---
+++ src/code_festival_2014_qualb/A/Main.java
@@ -0,0 +1,13 @@
+package code_festival_2014_qualb.A;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Math.max(Integer.parseInt(sc.next()), Integer.parseInt(sc.next())));
+
+	}
+}
