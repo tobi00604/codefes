084d09a998bc785edd2f66938d8b8be1832d1529
tobi00604 <>
2021-05-14 12:38:02 +0900

    A - CODE FESTIVAL 2015

---
+++ src/code_festival_2015_quala/A/Main.java
@@ -0,0 +1,13 @@
+package code_festival_2015_quala.A;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next().replace("4", "5"));
+
+	}
+}
