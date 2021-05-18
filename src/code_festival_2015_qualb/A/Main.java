ee2f3dd1fdaa6e01a690f8a151a4aaa2e4e0ae4b
tobi00604 <>
2021-05-18 19:08:34 +0900

    A - ダブル文字列

---
+++ src/code_festival_2015_qualb/A/Main.java
@@ -0,0 +1,20 @@
+package code_festival_2015_qualb.A;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		s = "";
+		for (int i = 'a'; i <= 'z'; i++) {
+			s += (char) i;
+		}
+		for (int i = 'a'; i <= 'z'; i++) {
+			s += (char) i;
+		}
+		System.out.println(s);
+	}
+}
