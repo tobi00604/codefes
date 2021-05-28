897d0ac8fbf635d14ad8f2fe4d12bf5d7d7edd77
tobi00604 <>
2021-05-28 18:48:00 +0900

    A - XXFESTIVAL

---
+++ src/code_festival_2017_qualb/A/Main.java
@@ -0,0 +1,13 @@
+package code_festival_2017_qualb.A;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s.substring(0, s.length() - 8));
+	}
+}
