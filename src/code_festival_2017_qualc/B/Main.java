a15dbaa01b9d087e654c71289956ee5554526804
tobi00604 <>
2021-06-29 19:19:13 +0900

    B - Similar Arrays

---
+++ src/code_festival_2017_qualc/B/Main.java
@@ -0,0 +1,27 @@
+package code_festival_2017_qualc.B;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// “ü—Í
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(sc.next());
+		}
+
+		int entire = 1;
+		int temp = 1;
+		for (int i = 0; i < n; i++) {
+			entire *= 3;
+			temp *= (a[i] % 2 == 0) ? 2 : 1;
+		}
+
+		System.out.println(entire - temp);
+
+	}
+}
