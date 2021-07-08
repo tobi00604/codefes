8c20f413330cee630df5aeadfb9da3e1c3469d64
tobi00604 <>
2021-07-08 20:24:33 +0900

    C - 旅館

---
+++ src/code_festival_2015_qualb/C/Main.java
@@ -0,0 +1,47 @@
+package code_festival_2015_qualb.C;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// 入力
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		int m = Integer.parseInt(sc.next());
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Long.parseLong(sc.next());
+		}
+		long[] b = new long[m];
+		for (int i = 0; i < m; i++) {
+			b[i] = Long.parseLong(sc.next());
+		}
+		Arrays.sort(a);
+		Arrays.sort(b);
+
+		// 確認用
+//		System.out.println(Arrays.toString(a));
+//		System.out.println(Arrays.toString(b));
+
+		int c = 0;
+		for (int i = 0; i < m; i++) {
+			while (c < n && a[c] < b[i]) {
+				c++;
+			}
+
+			// 確認用
+//			System.out.println(c + " " + i);
+
+			if (c == n) {
+				System.out.println("NO");
+				return;
+			}
+			c++;
+		}
+
+		System.out.println("YES");
+
+	}
+}
